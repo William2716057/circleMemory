f8736f5c50170e0cba4121b010fc4073a714c3ae
William <willspri@gmail.com>
2024-09-12 10:30:00 +1200

    colour change function added

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 let currentCircle = 1;
 
+setTimeout(() => {
+    document.querySelectorAll('.circle').forEach(circle => {
+        circle.style.color = 'white';  //Change text colour to white to hide order
+    });
+}, 5000);
+
 document.querySelectorAll('.circle').forEach(circle => {
     circle.addEventListener('click', function () {
         const circleId = parseInt(this.id.replace('circle', ''));
 
         if (circleId === currentCircle) {
-            this.style.display = 'none';  // Hide the circle
+            this.style.display = 'none';  // Hide circle
             currentCircle++;
 
             if (currentCircle > 10) {
-                alert('Congratulations! You clicked all the circles in the correct order.');
+                alert('Well done!');
             }
         } else {
-            alert(`Click circle ${currentCircle} next!`);
+            //alert(`Click circle ${currentCircle} next!`);
+            alert(`Wrong! Game Over!`);
         }
     });
 });
